4b31a677d821b1af9d6ec7108f87d864158cd0e9
surgery <1115937017@qq.com>
2026-02-02 13:50:05 +0800

    TONGJI视觉调试专线

--- application/cmd/robot_cmd.c
+++ application/cmd/robot_cmd.c
@@ -168,10 +168,10 @@ double my_cos(double rad)
 void RobotCMDInit()
 {
     rc_data = RemoteControlInit(&huart3);   // 修改为对应串口,注意如果是自研板dbus协议串口需选用添加了反相器的那个，这个串口与我们的车一样
-    // usb_recv_data = USBVisionInit(&huart6); //这个不占用串口，把handle放里面完全是因为要消除警告
-    bubing_vision_recv_data = BubingVisionInit(&huart6); // 视觉通信串口，这个没问题
+
+    // bubing_vision_recv_data = BubingVisionInit(&huart6); // 视觉通信串口，这个没问题
     // daoohang_vision_recv_data = DaohangVisionInit(&huart6);
-    // usb_recv_data = USBVisionInit(&huart6);
+    usb_recv_data = USBVisionInit(&huart6);
 
     gimbal_cmd_pub = PubRegister("gimbal_cmd", sizeof(Gimbal_Ctrl_Cmd_s));
     gimbal_feed_sub = SubRegister("gimbal_feed", sizeof(Gimbal_Upload_Data_s));
@@ -454,9 +454,10 @@ static void AUTOKeySet()
     // gimbal_cmd_send.gimbal_mode = GIMBAL_AUTO_XUNLUO; //巡逻状态
     gimbal_behavior_to_motor();
     //自动瞄准模式
-    gimbal_cmd_send.pitch = bubing_vision_recv_data->pitch*angle_to_radian*PITCH_AUTO_SEN;
-    gimbal_cmd_send.yaw = bubing_vision_recv_data->yaw*angle_to_radian*YAW_AUTO_SEN;
-
+    // gimbal_cmd_send.pitch = bubing_vision_recv_data->pitch*angle_to_radian*PITCH_AUTO_SEN;
+    // gimbal_cmd_send.yaw = bubing_vision_recv_data->yaw*angle_to_radian*YAW_AUTO_SEN;
+    gimbal_cmd_send.pitch = usb_recv_data->pitch*angle_to_radian*PITCH_AUTO_SEN;
+    gimbal_cmd_send.yaw = usb_recv_data->yaw*angle_to_radian*YAW_AUTO_SEN;
     //自动巡逻模式云台
     // gimbal_cmd_send.pitch = 0;
     // gimbal_cmd_send.big_yaw = 0;
